331f461f638556b992cd4223b00cfdf1dac1c429
Mintoo Kumar <min2.kumar1111@gmail.com>
2025-12-15 19:47:13 +0530

    used APIView in Serializer.serializer

--- OTT_platform/api/urls.py
+++ OTT_platform/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from OTT_platform.api.view import movie_list, movie_details
+# from OTT_platform.api.view import movie_list, movie_details
+from OTT_platform.api.view import MovieListAPIView, MovieDetailsAPIView
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>/', movie_details, name='movie-details'),
+    # FBV
+    # path('list/', movie_list, name='movie-list'),
+    # path('<int:pk>/', movie_details, name='movie-details'),
+
+    # CBV - APIView
+    path('list/', MovieListAPIView.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetailsAPIView.as_view(), name='movie-details'),
 ]
